0a83ec802741c2c3519bc71a12b968620e32c00f
SKITTLE WAN (INFRA-ISD-OOCLL/HKG) <WANSK@OOCL.COM>
2018-11-26 15:24:39 +0800

    [wansk]publicToPrivate

---
+++ src/test/java/com/tws/refactoring/Policetest.java
@@ -0,0 +1,23 @@
+package com.tws.refactoring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PoliceTest
+{
+    @Test
+    void test_whether_driver_is_an_adult(){
+        Driver adultDriver = new Driver( 18 );
+        Driver notAdultDriver = new Driver( 15 );
+        Police police = new Police();
+
+
+
+        assertTrue(police.checkDriver( adultDriver ));
+        assertFalse( police.checkDriver( notAdultDriver ) );
+
+    }
+
+}
